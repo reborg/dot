6eb03bfbc145357b681bf3ec0ee84f9f6c5f33aa
reborg <reborg@reborg.net>
2011-11-22 04:51:42 +0000

    a few changes to the git info util

--- cmd/git-info.sh
+++ cmd/git-info.sh
@@ -15,24 +15,24 @@ while [ ! -d .git ] && [ ! `pwd` = "/" ]; do cd ..; done
 # Show various information about this git directory
 if [ -d .git ]; then
   echo "== Remote URL: `git remote -v`"
-
-  echo "== Remote Branches: "
-  git branch -r
   echo
+
+#  echo "== Remote Branches: "
+#  git branch -r
+#  echo
 
   echo "== Local Branches:"
   git branch
   echo
 
-  echo "== Configuration (.git/config)"
-  cat .git/config
-  echo
+#  echo "== Configuration (.git/config)"
+#  cat .git/config
+#  echo
 
   echo "== Most Recent Commit"
-  git &#8212;no-pager log &#8212;max-count=1
+  git --no-pager log --max-count=1
   echo
 
-  echo "Type 'git log' for more commits, or 'git show' for full commit details."
 else
   echo "Not a git repository."
 fi
