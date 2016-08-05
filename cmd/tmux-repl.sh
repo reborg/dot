05836ae18476f15181252e0e0e22aaab8c8685b5
reborg <reborg@reborg.net>
2016-08-05 18:11:33 +0100

    latest and greatest

---
+++ cmd/tmux-repl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+starttmux() {
+    echo
+    echo $HOSTS
+    if [ -z "$HOSTS" ]; then
+       echo -n "Please provide of list of hosts separated by spaces [ENTER]: "
+       read HOSTS
+    fi
+
+    tmux new-session -d -s $sessionname
+    for i in $HOSTS
+    do
+    tmux split-window -v -t $sessionname "cd /me/prj/dm/clj_fe; lein repl :connect $i:$PORT"
+    tmux select-layout tiled
+    done
+    tmux set-window-option synchronize-panes on
+    tmux attach -t $sessionname
+}
+
+BNAME=`basename $0`
+if  [ $# -lt 1 ]; then
+    usage
+    exit 0
+fi
+
+sessionname=$1
+starttmux
