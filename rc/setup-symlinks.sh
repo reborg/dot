ca3f4f118b57a449bd73c8458feaff476ac81b4f
reborg <reborg@reborg.net>
2016-08-25 15:04:07 +0000

    cleanup symlinks

--- rc/setup-symlinks.sh
+++ rc/setup-symlinks.sh
@@ -1,11 +1,10 @@
 ln -s /Users/reborg/prj/my/dot/rc/bash_profile .bash_profile
-ln -s /Users/reborg/Dropbox/Documents/home-dot-files/.netrc .netrc
 ln -s /Users/reborg/prj/my/dot/rc/gitconfig .gitconfig
 ln -s /Users/reborg/prj/my/dot/rc/tmux.conf .tmux.conf
 ln -s /Users/reborg/prj/my/dot/rc/ackrc .ackrc
 ln -s /Users/reborg/prj/my/dot/rc/vimrc .vimrc
 ln -s /Users/reborg/prj/my/dot/vim .vim
-ln -s /Users/reborg/prj/3rdparties/.emacs.d .emacs.d
+ln -s /Users/reborg/Dropbox/Documents/home-dot-files/.netrc .netrc
 ln -s /Users/reborg/Dropbox/Documents/home-dot-files/.ssh .ssh
 ln -s /Users/reborg/Dropbox/Documents/home-dot-files/.privaterc
 touch .hushlogin # suppress login message on new term
